19e1c86f6f3fb9fbee559cebb2c3a60523705482
Domagoj <domagoj.zivanovic@coreline.agency>
2020-02-26 17:51:49 +0100

    Initial

---
+++ transcoder/main.cpp
@@ -0,0 +1,14 @@
+extern "C"
+{
+#include <libavcodec/avcodec.h>
+#include <libavdevice/avdevice.h>
+#include <libavformat/avformat.h>
+#include <libavutil/avutil.h>
+}
+
+#include <iostream>
+
+int main()
+{
+    return 0;
+}
